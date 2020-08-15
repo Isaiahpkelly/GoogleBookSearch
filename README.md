58c1c81e0ed35d4ce7c00bd5dde14cb4eace0296
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-08-14 23:25:29 -0400

    Google Search Book

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # GoogleBookSearch
+
+
+## Overview
+
+In this activity, you'll create a new React-based Google Books Search app. This assignment requires you to create React components, work with helper/util functions, and utilize React lifecycle methods to query and display books based on user searches. You'll also use Node, Express and MongoDB so that users can save books to review or purchase later.
+
+## Starting the app locally
+
+Start by installing front and backend dependencies. While in this directory, run the following command:
+
+```
+npm install
+```
+
+This should install node modules within the server and the client folder.
+
+After both installations complete, run the following command in your terminal:
+
+```
+npm start
+```
+
+Your app should now be running on <http://localhost:3000>. The Express server should intercept any AJAX requests from the client.
+
+## Deployment (Heroku)
+
+To deploy, simply add and commit your changes, and push to Heroku. As is, the NPM scripts should take care of the rest.
---
+++ client/build/precache-manifest.e094e7b0e499702b91d89bbe31e24369.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca49d3327ced73ca62658dae6ee6a9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5399a8cf25f43f9f58c2",
+    "url": "/static/css/2.14feee9d.chunk.css"
+  },
+  {
+    "revision": "ec76819fd27c8c6f2800",
+    "url": "/static/css/main.11379dec.chunk.css"
+  },
+  {
+    "revision": "5399a8cf25f43f9f58c2",
+    "url": "/static/js/2.d91d3d2c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d91d3d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec76819fd27c8c6f2800",
+    "url": "/static/js/main.359314f9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
